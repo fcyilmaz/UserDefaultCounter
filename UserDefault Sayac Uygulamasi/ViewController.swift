a046d21e2fb462f8e03e2592a3117b1b5359d40b
Jones527 <fevzicanyilmaz@hotmail.com>
2021-10-29 13:54:46 +0300

    ilk commit

--- UserDefault Sayac Uygulamasi/ViewController.swift
+++ UserDefault Sayac Uygulamasi/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelCounter: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let d = UserDefaults.standard
+        
+        var counter = d.integer(forKey: "Counter")
+        
+        counter += 1
+        d.set(counter,forKey: "Counter")
+        
+        labelCounter.text = "Counter: \(counter)"
     }
 
 
